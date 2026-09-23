0408f4f95ed4beefd536d4e7b0562906f87fd792
devindirathuge <devindineosolution@gmail.com>
2026-09-23 08:13:52 +0530

    Create properties.php

---
+++ UxUi-Back/Variable/Product/properties.php
@@ -0,0 +1,20 @@
+<?php
+$lof_products = [
+    ['id' => 1,  'title' => 'MARGERINE',   'desc' => 'Specially formulated margarine for bakeries, hotels and food service.',                         'img' => 'Assets/Images/Product/properties/product-01.png', 'category' => 'margarine'],
+    ['id' => 2,  'title' => 'BAKERY FAT',  'desc' => 'Specially formulated margarine for bakeries, hotels and food service.',                          'img' => 'Assets/Images/Product/properties/product-02.png', 'category' => 'fat-ghee'],
+    ['id' => 3,  'title' => 'COCONUT OIL', 'desc' => 'Naturally processed coconut oil with exceptional purity and quality.',                           'img' => 'Assets/Images/Product/properties/product-03.png', 'category' => 'cooking-oil'],
+    ['id' => 4,  'title' => 'EDIBLE OIL',  'desc' => 'Premium refined edible palm olein for everyday cooking and food production.',                    'img' => 'Assets/Images/Product/properties/product-04.png', 'category' => 'cooking-oil'],
+    ['id' => 5,  'title' => 'BAKERY FAT',  'desc' => 'Specialty formulated shortening and fats for bakeries and commercial food production.',          'img' => 'Assets/Images/Product/properties/product-01.png', 'category' => 'fat-ghee'],
+    ['id' => 6,  'title' => 'MARGERINE',   'desc' => 'Specially formulated margarine for bakeries, hotels and food service.',                          'img' => 'Assets/Images/Product/properties/product-01.png', 'category' => 'margarine'],
+    ['id' => 7,  'title' => 'COCONUT OIL', 'desc' => 'Naturally processed coconut oil with exceptional purity and quality.',                           'img' => 'Assets/Images/Product/properties/product-01.png', 'category' => 'cooking-oil'],
+    ['id' => 8,  'title' => 'EDIBLE OIL',  'desc' => 'Premium refined edible palm olein for everyday cooking and food production.',                    'img' => 'Assets/Images/Product/properties/product-01.png', 'category' => 'cooking-oil'],
+    ['id' => 9,  'title' => 'COCONUT OIL', 'desc' => 'Naturally processed coconut oil with exceptional purity and quality.',                           'img' => 'Assets/Images/Product/properties/product-01.png', 'category' => 'cooking-oil'],
+    ['id' => 10, 'title' => 'EDIBLE OIL',  'desc' => 'Premium refined edible palm olein for everyday cooking and food production.',                    'img' => 'Assets/Images/Product/properties/product-01.png', 'category' => 'cooking-oil'],
+    ['id' => 11, 'title' => 'BAKERY FAT',  'desc' => 'Specialty formulated shortening and fats for bakeries and commercial food production.',          'img' => 'Assets/Images/Product/properties/product-01.png', 'category' => 'fat-ghee'],
+    ['id' => 12, 'title' => 'MARGERINE',   'desc' => 'Specially formulated margarine for bakeries, hotels and food service.',                          'img' => 'Assets/Images/Product/properties/product-01.png', 'category' => 'margarine'],
+    ['id' => 13, 'title' => 'EDIBLE OIL',  'desc' => 'Premium refined edible palm olein for everyday cooking and food production.',                    'img' => 'Assets/Images/Product/properties/product-01.png', 'category' => 'cooking-oil'],
+    ['id' => 14, 'title' => 'COCONUT OIL', 'desc' => 'Naturally processed coconut oil with exceptional purity and quality.',                           'img' => 'Assets/Images/Product/properties/product-01.png', 'category' => 'cooking-oil'],
+    ['id' => 15, 'title' => 'BAKERY FAT',  'desc' => 'Specialty formulated shortening and fats for bakeries and commercial food production.',          'img' => 'Assets/Images/Product/properties/product-01.png', 'category' => 'fat-ghee'],
+    ['id' => 16, 'title' => 'MARGERINE',   'desc' => 'Specially formulated margarine for bakeries, hotels and food service.',                          'img' => 'Assets/Images/Product/properties/product-01.png', 'category' => 'margarine'],
+];
+?>

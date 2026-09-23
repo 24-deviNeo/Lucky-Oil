9f481c3cbe7608e5309bb2901cb86f4204776271
devindirathuge <devindineosolution@gmail.com>
2026-09-23 06:40:30 +0530

    Update AdverticementShower.php

---
+++ UxUi-Back/Variable/Product/AdverticementShower.php
@@ -0,0 +1,34 @@
+<?php
+$advertisement_slides = [
+    [
+        'title' => 'PURE GOLD REFINED COOKING OIL',
+        'image' => 'Assets/Images/Product/Advertising/png 1.jpg',
+        'alt'   => 'Quality Inspection'
+    ],
+    [
+        'title' => 'VIRGIN COCONUT & REFINED OILS',
+        'image' => 'Assets/Images/Product/Advertising/png 2.jpg',
+        'alt'   => 'Pure Organic Coconut Oil'
+    ],
+    [
+        'title' => 'COMMERCIAL BAKERY FATS & MARGARINE',
+        'image' => 'Assets/Images/Product/Advertising/png 3.jpg',
+        'alt'   => 'Bakery Ingredients'
+    ],
+    [
+        'title' => 'SUSTAINABLE INDUSTRIAL REFINERY',
+        'image' => 'Assets/Images/Product/Advertising/png 4.jpg',
+        'alt'   => 'Advanced Manufacturing Facility'
+    ]
+];
+
+$advertisement_action = [
+    'label' => 'LEARN MORE',
+    'href'  => 'about.php'
+];
+
+$advertisement_shower = [
+    'slides' => $advertisement_slides,
+    'action' => $advertisement_action
+];
+?>
